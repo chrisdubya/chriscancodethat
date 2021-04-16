f294d5e616e6d4e75d330e2e7ea8b7b94b69c2de
Chris Williams <chris.r.williams@icloud.com>
2021-04-15 23:51:23 -0400

    update package deps

--- config/common.js
+++ config/common.js
@@ -1,90 +1,93 @@
-const path = require('path');
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const __root = path.resolve(__dirname, '../');
+const __root = path.resolve(__dirname, "../");
 
 module.exports = {
 	entry: {
-		index: ['@babel/polyfill', './src/scripts/index.js'],
+		index: ["@babel/polyfill", "./src/scripts/index.js"],
 	},
 	output: {
-		path: path.resolve(__root, 'docs'),
-		filename: 'bundle.js'
+		path: path.resolve(__root, "docs"),
+		filename: "bundle.js",
 	},
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
 				use: {
-					loader: 'babel-loader',
+					loader: "babel-loader",
 					options: {
-						presets: ['@babel/preset-env'],
-						plugins: ['@babel/plugin-syntax-dynamic-import']
-					}
+						presets: ["@babel/preset-env"],
+						plugins: ["@babel/plugin-syntax-dynamic-import"],
+					},
 				},
-				exclude: /node_modules/
+				exclude: /node_modules/,
 			},
 			{
 				test: /\.(glsl|frag|vert)$/,
-				use: ['glslify-import-loader', 'raw-loader', 'glslify-loader']
+				use: ["glslify-import-loader", "raw-loader", "glslify-loader"],
 			},
 			{
 				test: /three\/examples\/js/,
-				use: 'imports-loader?THREE=three'
+				use: "imports-loader?THREE=three",
 			},
-	    {
+			{
 				test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
-	    }
-		]
+				use: [
+					{
+						// Adds CSS to the DOM by injecting a `<style>` tag
+						loader: "style-loader",
+					},
+					{
+						// Interprets `@import` and `url()` like `import/require()` and will resolve them
+						loader: "css-loader",
+					},
+					{
+						// Loader for webpack to process CSS with PostCSS
+						loader: "postcss-loader",
+						options: {
+							plugins: function () {
+								return [require("autoprefixer")];
+							},
+						},
+					},
+					{
+						// Loads a SASS/SCSS file and compiles it to CSS
+						loader: "sass-loader",
+					},
+				],
+			},
+			{
+				test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				use: [
+					{
+						loader: "file-loader",
+					},
+				],
+			},
+		],
 	},
 	resolve: {
 		alias: {
-			'three-examples': path.join(__root, './node_modules/three/examples/js'),
-		}
+			"three-examples": path.join(__root, "./node_modules/three/examples/js"),
+		},
 	},
 	plugins: [
-		new CleanWebpackPlugin(
-			['docs'],
-			{ root: __root },
-		),
+		new CleanWebpackPlugin(["docs"], { root: __root }),
 		new CopyWebpackPlugin([
 			{
-				from: path.resolve(__root, 'static'),
-			}
+				from: path.resolve(__root, "static"),
+			},
 		]),
 		new HtmlWebpackPlugin({
-			template: './src/index.html',
+			template: "./src/index.html",
 		}),
 		new webpack.ProvidePlugin({
-			'THREE': 'three'
-		})
-	]
+			THREE: "three",
+		}),
+	],
 };
